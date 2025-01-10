2eab8b198c5881e3d7f60d699318f50791e9a11e
ruisantiago <ruisantiagomr@gmail.com>
2025-01-10 15:38:22 +0000

    next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
