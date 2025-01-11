134a0eeae1ba2e06782f5374bf68a821a5c4b9aa
ruisantiago <ruisantiagomr@gmail.com>
2025-01-11 14:36:50 +0000

    fix versions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,10 @@
 import WalletProvider from "@/providers/WalletProvider";
 import "@near-wallet-selector/modal-ui/styles.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Bitte AI Chat Boilerplate",
@@ -27,11 +19,7 @@ export default function RootLayout({
   return (
     <WalletProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
+        <body className={`${inter.className} antialiased`}>{children}</body>
       </html>
     </WalletProvider>
   );
